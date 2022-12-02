08f29656d29886654644fae820fd7a180eecc532
DavSladk <davsladk+github@gmail.com>
2022-12-02 19:30:19 +0100

    Scripts done.

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-placeholder
+#!/bin/bash
+FULL_PATH=$(realpath $0)
+DIR_PATH=$(dirname $FULL_PATH)
+ENV="${DIR_PATH}/venv"
+ACTIVE="${ENV}/bin/activate"
+REQ="${DIR_PATH}/requirements.txt"
+
+python3 -m venv $ENV
+source $ACTIVE
+pip install -r $REQ
+deactivate
--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
-placeholder
+#!/bin/bash
+FULL_PATH=$(realpath $0)
+DIR_PATH=$(dirname $FULL_PATH)
+ENV="${DIR_PATH}/venv"
+ACTIVE="${ENV}/bin/activate"
+REQ="${DIR_PATH}/requirements.txt"
+URLS="${DIR_PATH}/urls.py"
+DATA="${DIR_PATH}/data.py"
+
+source $ACTIVE
+python3 $URLS -n 20 | python3 $DATA
+deactivate
